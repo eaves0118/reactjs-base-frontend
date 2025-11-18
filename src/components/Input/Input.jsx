792fc3eacaa795f2080ff694d038a617255614b5
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-18 15:34:45 +0700

    done ux ui login register

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style.module.scss";
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
 const Input = ({ type = "text", placeholder }) => {
-  return <input className={styles.inputCommon} type={type} placeholder={placeholder} />;
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+  const isShowTextOrPassword = type === "password" && showPassword ? "text" : type;
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div className={styles.input__container}>
+      <input className={styles.inputCommon} type={isShowTextOrPassword} placeholder={placeholder} />
+      {isPassword && (
+        <div className={styles.secret} onClick={handleShowPassword}>
+          {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Input;
